8e5c44ffee0a1063738a2e024ba8f802cc77bc34
Devin Rousso <dcrousso@gmail.com>
2016-03-11 23:24:22 -0800

    Minor header style adjustments

--- src/Dots/AuthenticationServlet.java
+++ src/Dots/AuthenticationServlet.java
@@ -14,10 +14,11 @@ public class AuthenticationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getSession().getAttribute("authenticated") == null)
+		if (request.getSession().isNew() || request.getSession().getAttribute("authenticated") == null)
 			return;
 
 		request.getSession().removeAttribute("authenticated");
+		request.getSession().invalidate();
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
